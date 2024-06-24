08484382a3b912fd6a8bf7579a7cfd3381611360
Actaviys <163467524+Actaviys@users.noreply.github.com>
2024-06-24 21:06:02 +0300

    comit_test1

--- commit_test.py
+++ commit_test.py
@@ -1,4 +1,78 @@
-print("\n","Dima:")
 
-#тест
-print("\t","TEST_commit")
+
+# my_list = [1, 2, 3, 4, 2, 2, 5, 5, 2]
+# count_2 = my_list.count(int(input("Number: ")))
+# print(count_2)  # Виведе 4, оскільки число 2 зустрічається 4 рази
+
+
+
+# # Встановлюємо ціни на продукти
+# price_per_croissant = 1.04
+# price_per_glass = 0.34
+# price_per_coffee_pack = 4.42
+
+# # Кількість кожного продукту
+# num_croissants = int(input("Введіть кількість круасанів: "))
+# num_glasses = int(input("Введіть кількість склянок: "))
+# num_coffee_packs = int(input("Введіть кількість упаковок кави: "))
+
+# # Обчислення загальної вартості
+# total_cost = num_croissants * price_per_croissant + \
+#              num_glasses * price_per_glass + \
+#              num_coffee_packs * price_per_coffee_pack
+
+# # Визначаємо кількість повних доларів і центів
+# total_dollars = int(total_cost)
+# total_cents = int(total_cost * 100)
+
+# # Вивід результату
+# print(f"Загальна вартість у повних доларах: {total_dollars} доларів")
+# print(f"Загальна вартість у центах: {total_cents} центів")
+
+
+
+
+# num = 5  # приклад значення для num
+
+# if num > 10:
+#     print("num більше за 10")
+# else:
+#     print("num не більше за 10")
+
+
+# a = input('Введіть число: ')
+# a = int(a)
+
+# if a > 0:
+#     print('Число додатне:', a)
+# if a == 1:
+#     print('Число дорівнює 1')
+# else:
+#     print("a != 0")
+
+
+# menu = ["wraps", "sandwiches", "soup", "salad"]
+# for i in menu:
+#      print(i)
+
+
+#var1 = 12 + 34
+#var1 = 46
+
+
+#word = "hello"
+#var2 = len("hello")
+# var2 = 5
+ 
+ 
+#name = "Wendy\'s dog"
+#print(name)
+
+
+#a = float(input("Enter number1: "))
+#b = float(input("Enter number2: "))
+#print(a+b)
+
+#print("\n","Dima:")
+
+#print("\t","TEST_commit")
